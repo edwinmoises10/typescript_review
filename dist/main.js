eb093c8904a847ff7c0e5da9aa53b924dc90f79a
Edwin Moises Oña <edwinmoisesona@Edwins-MacBook-Pro.local>
2025-11-19 10:49:46 -0500

    first steps in TS

---
+++ dist/main.js
@@ -0,0 +1,18 @@
+"use strict";
+//Se puede usar ("root") as HTMLElement ò ("root")! (TS Infiere el tipo)
+//Recomendado el bloque if actua como guardia de tipo
+// let username: string = 'Hello Moises'
+// const resultDOM =  document.getElementById("root")
+// if(resultDOM){
+//     resultDOM.innerHTML= `<h2> ${username} </h2>`
+// }else{
+//     console.log("DOM Not Found")
+// }
+//tipo de datos 
+let movie = "Batman";
+let duration = 200;
+let hasOscar = true;
+movie = "Batman - The Dark Kning";
+duration = 240;
+hasOscar = false;
+console.log(movie, duration, hasOscar);
