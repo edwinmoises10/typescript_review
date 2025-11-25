3cc492e90ecee20fa0c95558241aa3af8e7453d6
Edwin Moises Oña <edwinmoisesona@Edwins-MacBook-Pro.local>
2025-11-25 11:14:26 -0500

    review2_ISP_download_latency_done

--- dist/components/review2.js
+++ dist/components/review2.js
@@ -1,10 +1,11 @@
-"use strict";
+import Swal from "sweetalert2";
 /// INPUTS
 const ispName = document.querySelector("#ispName");
 const downloadSpeed = document.querySelector("#downloadSpeed");
 const latencyMs = document.querySelector("#latencyMs");
 ///BUTTON
 const getInformation = document.querySelector("#getInformation");
+const btClearDB = document.querySelector("#btClearDB");
 // let coreDB: arr = []
 let getDbData = localStorage.getItem("coredb");
 let coreDB = getDbData ? JSON.parse(getDbData) : [];
@@ -20,9 +21,94 @@ const getInformationISP = () => {
                 latencyMs: getLatencyInput,
             };
             coreDB.push(data);
+            getTestData();
             localStorage.setItem("coredb", JSON.stringify(coreDB));
-            console.log(coreDB);
+            // ¡Limpiamos los inputs!
+            if (ispName && downloadSpeed && latencyMs) {
+                ispName.value = "";
+                downloadSpeed.value = "";
+                latencyMs.value = "";
+            }
         };
     }
 };
+const clearDB = () => {
+    if (btClearDB) {
+        btClearDB.onclick = () => {
+            if (coreDB.length == 0) {
+                Swal.fire("Core DB Clear");
+                return;
+            }
+            console.log("Click DB ");
+            const swalWithBootstrapButtons = Swal.mixin({
+                customClass: {
+                    confirmButton: "btn btn-success",
+                    cancelButton: "btn btn-danger",
+                },
+                buttonsStyling: false,
+            });
+            swalWithBootstrapButtons
+                .fire({
+                title: "Are you sure to delete DB Users?",
+                text: "You won't be able to revert this!",
+                icon: "warning",
+                showCancelButton: true,
+                confirmButtonText: "Yes, delete it!",
+                cancelButtonText: "No, cancel!",
+                reverseButtons: true,
+            })
+                .then((result) => {
+                if (result.isConfirmed) {
+                    localStorage.removeItem("coredb");
+                    coreDB.length = 0;
+                    getTestData();
+                    coreDB = [];
+                    swalWithBootstrapButtons.fire({
+                        title: "Deleted!",
+                        text: "Your DB has been deleted.",
+                        icon: "success",
+                    });
+                }
+                else if (
+                /* Read more about handling dismissals below */
+                result.dismiss === Swal.DismissReason.cancel) {
+                    swalWithBootstrapButtons.fire({
+                        title: "Cancelled",
+                        text: "Your DB is safe :)",
+                        icon: "error",
+                    });
+                }
+            });
+        };
+    }
+};
+function getTestData() {
+    let result = "";
+    const testData = document.querySelector("#testData");
+    coreDB.forEach((test) => {
+        result += `
+    <tr>
+        <td>${test.internetServiceProvider}</td>
+        <td>${test.downloadSpeedMbps}</td>
+        <td>${test.latencyMs}</td>
+        <td>${getStatus(test.latencyMs)}</td>
+    </tr>
+        `;
+    });
+    if (testData) {
+        testData.innerHTML = result;
+    }
+}
+const getStatus = (latencyValue) => {
+    if (latencyValue >= 50) {
+        let statusLatencyMs = "Stable Latency";
+        return statusLatencyMs;
+    }
+    else {
+        let statusLatencyMs1 = "Unstable Latency";
+        return statusLatencyMs1;
+    }
+};
 getInformationISP();
+getTestData();
+clearDB();
--- src/components/review2.ts
+++ src/components/review2.ts
@@ -1,3 +1,5 @@
+import Swal from "sweetalert2";
+
 /// INPUTS
 const ispName = document.querySelector<HTMLInputElement>("#ispName");
 const downloadSpeed =
@@ -6,12 +8,17 @@ const latencyMs = document.querySelector<HTMLInputElement>("#latencyMs");
 ///BUTTON
 const getInformation =
   document.querySelector<HTMLButtonElement>("#getInformation");
+const btClearDB = document.querySelector<HTMLButtonElement>("#btClearDB");
 
 //ARRAY
 
-type arr = object[];
+// type arr = object[]; Caja negra vacia se que es obj pero no se que
+//proiedades tiene
 
-type getElements = {
+type arr = NetworkTest[]; // Sera un objeto pero con las siguientes
+//caracteristicas
+
+type NetworkTest = {
   internetServiceProvider: string;
   downloadSpeedMbps: number;
   latencyMs: number;
@@ -25,21 +32,118 @@ let coreDB: arr = getDbData ? JSON.parse(getDbData) : [];
 const getInformationISP = () => {
   if (getInformation) {
     getInformation.onclick = () => {
-        
       let getStrInput = String(ispName?.value);
       let getDownloadInput = Number(downloadSpeed?.value);
       let getLatencyInput = Number(latencyMs?.value);
 
-      let data: getElements = {
+      let data: NetworkTest = {
         internetServiceProvider: getStrInput,
         downloadSpeedMbps: getDownloadInput,
         latencyMs: getLatencyInput,
       };
       coreDB.push(data);
+      getTestData();
       localStorage.setItem("coredb", JSON.stringify(coreDB));
-      console.log(coreDB);
+
+      // ¡Limpiamos los inputs!
+      if (ispName && downloadSpeed && latencyMs) {
+        ispName.value = "";
+        downloadSpeed.value = "";
+        latencyMs.value = "";
+      }
     };
   }
 };
 
+const clearDB = () => {
+  if (btClearDB) {
+    btClearDB.onclick = () => {
+      if (coreDB.length == 0) {
+        Swal.fire("Core DB Clear");
+        return;
+      }
+
+      console.log("Click DB ");
+
+      const swalWithBootstrapButtons = Swal.mixin({
+        customClass: {
+          confirmButton: "btn btn-success",
+          cancelButton: "btn btn-danger",
+        },
+        buttonsStyling: false,
+      });
+      swalWithBootstrapButtons
+        .fire({
+          title: "Are you sure to delete DB Users?",
+          text: "You won't be able to revert this!",
+          icon: "warning",
+          showCancelButton: true,
+          confirmButtonText: "Yes, delete it!",
+          cancelButtonText: "No, cancel!",
+          reverseButtons: true,
+        })
+        .then((result) => {
+          if (result.isConfirmed) {
+            localStorage.removeItem("coredb");
+            coreDB.length = 0;
+            getTestData();
+            coreDB = [];
+
+            swalWithBootstrapButtons.fire({
+              title: "Deleted!",
+              text: "Your DB has been deleted.",
+              icon: "success",
+            });
+          } else if (
+            /* Read more about handling dismissals below */
+            result.dismiss === Swal.DismissReason.cancel
+          ) {
+            swalWithBootstrapButtons.fire({
+              title: "Cancelled",
+              text: "Your DB is safe :)",
+              icon: "error",
+            });
+          }
+        });
+    };
+  }
+};
+
+function getTestData() {
+  type result = string;
+  let result: result = "";
+
+  const testData = document.querySelector<HTMLTableElement>("#testData");
+
+  coreDB.forEach((test) => {
+    result += `
+    <tr>
+        <td>${test.internetServiceProvider}</td>
+        <td>${test.downloadSpeedMbps}</td>
+        <td>${test.latencyMs}</td>
+        <td>${getStatus(test.latencyMs)}</td>
+    </tr>
+        `;
+  });
+  if (testData) {
+    testData.innerHTML = result;
+  }
+}
+
+const getStatus = (latencyValue: number) => {
+  type stable = "Stable Latency";
+  type unstable = "Unstable Latency";
+  type statusLatency = stable | unstable;
+
+  if (latencyValue >= 50) {
+    let statusLatencyMs: statusLatency = "Stable Latency";
+    return statusLatencyMs;
+  } else {
+    let statusLatencyMs1: statusLatency = "Unstable Latency";
+    return statusLatencyMs1;
+  }
+};
+
 getInformationISP();
+getTestData();
+clearDB();
